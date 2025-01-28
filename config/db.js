aa5013740ae08221bdad82fe5c25f09e7670fbeb
jemisha <ghadiyajemisha04@gmail.com>
2025-01-28 12:14:34 +0530

    six commit

--- config/db.js
+++ config/db.js
@@ -4,12 +4,17 @@ const { Client } = require("pg");
 require("dotenv").config();
 
 const db = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  }
+
   // const db = new pg.Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  // user: process.env.DB_USER,
+  // host: process.env.DB_HOST,
+  // database: process.env.DB_NAME,
+  // password: process.env.DB_PASSWORD,
+  // port: process.env.DB_PORT,
 });
 
 // db.connect()
